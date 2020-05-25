5d7a6b52d4f511b1eed8c4d3f03580369d0c8034
ChenYuqi <Chenyq@mail.ustc.edu.cn>
2020-05-26 01:31:50 +0800

    Update

--- defenses/adversarial_train.py
+++ defenses/adversarial_train.py
@@ -3,6 +3,8 @@ import attacks.attack
 import torch
 import torch.nn.functional as F
 
+import random
+
 def adv_train(model, attack, device, train_loader, optimizer, epoch):
     model.train()
     train_loss = 0.0
@@ -52,21 +54,29 @@ def adv_guide_train(model, device, train_loader, guide_sets, optimizer, epoch, b
         output = model(data)
         # adv_data = attack.generate(model, data, target)
 
+        # target selecting
+        # randomly select a target label and calculate the adversarial perturbation
+        target_label = target
+        while target_label == target:
+            target_label = random.randint(0, 9)
         data_copy = data.clone().detach().requires_grad_(True)
         output_copy = model(data_copy)
-        L1 = F.nll_loss(output_copy, target)
+        L1 = F.nll_loss(output_copy, target_label)
         # print(torch.autograd.grad(L1, data_copy, create_graph=True)[0])
-        adv_pertur = epsilon * torch.autograd.grad(L1, data_copy, create_graph=True)[0]
+        adv_pertur = - epsilon * torch.autograd.grad(L1, data_copy, create_graph=True)[0]
 
-        adv_data = data.clone().detach() + adv_pertur.clone().detach()
-        adv_output = model(adv_data)
-        adv_pred = adv_output.argmax(dim=1, keepdim=True)
-        guide_data, _ = guide_sample(guide_sets, adv_pred)
-        guide_data = guide_data.to(device)
+        # adv_data = data.clone().detach() + adv_pertur.clone().detach()
+        # adv_output = model(adv_data)
+        # adv_pred = adv_output.argmax(dim=1, keepdim=True)
+        # guide_data, _ = guide_sample(guide_sets, adv_pred)
+        # guide_data = guide_data.to(device)
         
+        guide_data, _ = guide_sample(guide_sets, target_label)
+        guide_data = guide_data.to(device)
+
         train_loss = F.nll_loss(output, target)
-        # guided_loss = F.mse_loss(adv_pertur, data - guide_data)
-        guided_loss = F.kl_div(adv_pertur, data - guide_data)
+        guided_loss = F.mse_loss(adv_pertur, data - guide_data)
+        # guided_loss = F.kl_div(adv_pertur, data - guide_data)
         loss = (1-beta)*train_loss + beta*guided_loss
         # loss = F.nll_loss(output, target) + F.mse_loss(data - guide_data, adv_pertur)
         loss.backward()
@@ -80,5 +90,5 @@ def adv_guide_train(model, device, train_loader, guide_sets, optimizer, epoch, b
     train_loss_sum /= len(train_loader)
     guided_loss_sum /= len(train_loader)
 
-    print('Train Epoch: {} \tLoss: {:.6f}, Training Loss: {:.6f}, Guied Loss: {:.6f}'.format(
+    print('Train Epoch: {} \tLoss: {:.6f}, Training Loss: {:.6f}, Guided Loss: {:.6f}'.format(
             epoch, loss_sum, train_loss_sum, guided_loss_sum))
