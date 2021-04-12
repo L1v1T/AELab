fix: update normalization in AGT

defenses/adversarial_train.py
@@ -45,37 +45,16 @@ def adv_guide_train(model, device, train_loader, guide_sets, optimizer, epoch,
         labels = torch.tensor(labels)
 
         return databatch, labels
-    
-    # def l2_regular_loss(model, device):
-    #     loss = None
-    #     n = 0
-    #     for name, param in model.named_parameters():
-    #         if 'weight' in name:
-    #             if loss is None:
-    #                 loss = F.mse_loss(param, 
-    #                                 torch.zeros(param.size()).to(device), 
-    #                                 reduction='sum')
-    #             else:
-    #                 loss = loss + F.mse_loss(param, 
-    #                                         torch.zeros(param.size()).to(device), 
-    #                                         reduction='sum')
-    #             n += param.numel()
 
-    #     return loss / (2 * n)
-
-    class LayerActivations:
-        features = None
-    
-        def __init__(self, model):
-            self.hook = model.conv2.register_forward_hook(self.hook_fn)
-
-        def hook_fn(self, module, input, output):
-            self.features = output
-    
-        def remove(self):
-            self.hook.remove()
-    
-    # penultimate_layer = LayerActivations(model)
+    def aux_targets_gen(target):
+        aux_target = torch.empty((9, 0), dtype=torch.int)
+        for i in range(len(target)):
+            target_list = list(range(10))
+            target_list.remove(target[i])
+            random.shuffle(target_list)
+            target_tensor = torch.tensor(target_list).unsqueeze(dim=1)
+            aux_target = torch.cat((aux_target, target_tensor), dim=-1)
+        return aux_target
 
     loss_sum = 0.0
     train_loss_sum = 0.0
@@ -86,76 +65,51 @@ def adv_guide_train(model, device, train_loader, guide_sets, optimizer, epoch,
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
-        # adv_data = attack.generate(model, data, target)
 
         # target selecting
-        # randomly select a target label and calculate the adversarial perturbation
-        target_label = target.clone().detach()
-        for i in range(len(target_label)):
-            while target_label[i] == target[i]:
-                target_label[i] = random.randint(0, 9)
-        data_copy = data.clone().detach().requires_grad_(True)
-        output_copy = model(data_copy)
-        L1 = F.nll_loss(output_copy, target_label)
-        # print(torch.autograd.grad(L1, data_copy, create_graph=True)[0])
-        adv_pertur = - torch.autograd.grad(L1, data_copy, create_graph=True)[0]
-        min = torch.min(adv_pertur)
-        max = torch.max(adv_pertur)
-        mid = (max + min) / 2
-        zero_mean = (max - min) / 2
-        adv_pertur_norm = epsilon * (adv_pertur - mid) / zero_mean
-        adv_data = data.clone().detach() + adv_pertur_norm
-
-        # adv_pertur_norm = - epsilon * torch.tanh(torch.autograd.grad(L1, data_copy, create_graph=True)[0])
-
-        # adv_data = data.clone().detach() + adv_pertur.clone().detach()
-        # adv_output = model(adv_data)
-        # adv_pred = adv_output.argmax(dim=1, keepdim=True)
-        # guide_data, _ = guide_sample(guide_sets, adv_pred)
-        # guide_data = guide_data.to(device)
+        # generate adversarial perturbation for 9 target
+        aux_targets = aux_targets_gen(target)
+        guided_loss = 0.0
+        for target_label in aux_targets:
+            target_label = target_label.to(device)
+            data_copy = data.clone().detach().requires_grad_(True)
+            output_copy = model(data_copy)
+            L1 = F.nll_loss(output_copy, target_label)
+            adv_pertur = - torch.autograd.grad(L1, data_copy, create_graph=True)[0]
+            # normalizing adversarial perturbation
+            adv_max = torch.max(torch.max(adv_pertur, dim=-1)[0], dim=-1)[0]
+            adv_min = torch.min(torch.min(adv_pertur, dim=-1)[0], dim=-1)[0]
+            adv_mid = (adv_max + adv_min) / 2
+            adv_zero = (adv_max - adv_min) / 2
+            adv_norm = torch.ones_like(adv_pertur)
+            for i in range(len(adv_pertur)):
+                for j in range(len(adv_pertur[0])):
+                    adv_norm[i][j] = (adv_pertur[i][j] - adv_mid[i][j].item()) / adv_zero[i][j].item()
+            adv_norm = epsilon * adv_norm
+            adv_data = data.clone().detach() + adv_norm
         
-        guide_data, _ = guide_sample(guide_sets, target_label)
-        guide_data = guide_data.to(device)
-
-        # model(adv_data)
-        # adv_features = penultimate_layer.features
-        # model(guide_data)
-        # guide_features = penultimate_layer.features
+            guide_data, _ = guide_sample(guide_sets, target_label)
+            guide_data = guide_data.to(device)
+            
+            guided_loss = F.mse_loss(adv_data, guide_data) + guided_loss
 
         train_loss = F.nll_loss(output, target)
-        # guided_loss = F.mse_loss(adv_pertur_norm, guide_data - data)
-        guided_loss = F.mse_loss(adv_data, guide_data)
-        # guided_loss = F.mse_loss(adv_features, guide_features)
-        # regular_loss = l2_regular_loss(model, device)
-        # adv_regular_loss = F.mse_loss(adv_pertur, torch.zeros(adv_pertur.size()).to(device))
-        # guided_loss = F.kl_div(adv_pertur_norm, guide_data - data)
-        # loss = (1-beta)*train_loss + beta*guided_loss + weight_decay*regular_loss + gradient_decay*adv_regular_loss
         loss = (1-beta)*train_loss + beta*guided_loss
-        # loss = F.nll_loss(output, target) + F.mse_loss(guide_data - data, adv_pertur_norm)
         loss.backward()
         optimizer.step()
         loss_sum += loss.item()
         train_loss_sum += train_loss.item()
-        # regular_loss_sum += regular_loss.item()
-        # adv_regular_loss_sum += adv_regular_loss.item()
         guided_loss_sum += guided_loss.item()
 
 
     loss_sum /= len(train_loader)
     train_loss_sum /= len(train_loader)
-    # regular_loss_sum /= len(train_loader)
-    # adv_regular_loss_sum /= len(train_loader)
     guided_loss_sum /= len(train_loader)
 
-    # penultimate_layer.remove()
 
     print('Train Epoch: {} \tLoss: {:.6f}, Training Loss: {:.6f}, Guided Loss: {:.6f}'.format(
             epoch, loss_sum, train_loss_sum, guided_loss_sum))
 
-#     print('Train Epoch: {} \tLoss: {:.6f}, Training Loss: {:.6f}, \
-# Gradient Regularization Loss: {:.3e}, L2 Regularization Loss: {:.3e}, \
-# Guided Loss: {:.6f}'.format(
-#             epoch, loss_sum, train_loss_sum, adv_regular_loss_sum, regular_loss_sum, guided_loss_sum))
 
 
 def adv_guide_pgd_train(model, 
@@ -171,6 +125,16 @@ def adv_guide_pgd_train(model,
                         weight_decay, 
                         gradient_decay):
     model.train()
+
+    def aux_targets_gen(target):
+        aux_target = torch.empty((9, 0), dtype=torch.int)
+        for i in range(len(target)):
+            target_list = list(range(10))
+            target_list.remove(target[i])
+            random.shuffle(target_list)
+            target_tensor = torch.tensor(target_list).unsqueeze(dim=1)
+            aux_target = torch.cat((aux_target, target_tensor), dim=-1)
+        return aux_target
 
     def guide_sample(datasets, adv_pred):
         def sample(dataset):
@@ -202,41 +166,47 @@ def adv_guide_pgd_train(model,
 
         # target selecting
         # generate adversarial perturbation for 9 target
-        target_label = target.clone().detach()
-        for i in range(len(target_label)):
-            while target_label[i] == target[i]:
-                target_label[i] = random.randint(0, 9)
-        data_copy = data.clone().detach().requires_grad_(True)
-        output_copy = model(data_copy)
-        L1 = F.nll_loss(output_copy, target_label)
-        adv_pertur = - torch.autograd.grad(L1, data_copy, create_graph=True)[0]
+        # target_label = target.clone().detach()
+        aux_targets = aux_targets_gen(target)
+        guided_loss = 0
+        adv_loss = 0
+        for target_label in aux_targets:
+            target_label = target_label.to(device)
+            data_copy = data.clone().detach().requires_grad_(True)
+            output_copy = model(data_copy)
+            L1 = F.nll_loss(output_copy, target_label)
+            adv_pertur = - torch.autograd.grad(L1, data_copy, create_graph=True)[0]
+            
+            # normalizing adversarial perturbation
+            adv_max = torch.max(torch.max(adv_pertur, dim=-1)[0], dim=-1)[0]
+            adv_min = torch.min(torch.min(adv_pertur, dim=-1)[0], dim=-1)[0]
+            adv_mid = (adv_max + adv_min) / 2
+            adv_zero = (adv_max - adv_min) / 2
+            adv_norm = torch.ones_like(adv_pertur)
+            for i in range(len(adv_pertur)):
+                for j in range(len(adv_pertur[0])):
+                    adv_norm[i][j] = (adv_pertur[i][j] - adv_mid[i][j].item()) / adv_zero[i][j].item()
+            adv_norm = epsilon * adv_norm
+            # min = torch.min(adv_pertur)
+            # max = torch.max(adv_pertur)
+            # mid = (max + min) / 2
+            # zero_mean = (max - min) / 2
+            # adv_pertur_norm = epsilon * (adv_pertur - mid) / zero_mean
+            adv_data = data.clone().detach() + adv_norm
+
+            guide_data, _ = guide_sample(guide_sets, target_label)
+            guide_data = guide_data.to(device)
+
+            # adversarial example output
+            adv_output = model(attack.generate(model, data, target))
+            guided_loss = F.mse_loss(adv_data, guide_data) + guided_loss
+            adv_loss = attack.lf(adv_output, target) + adv_loss
+        # for i in range(len(target_label)):
+        #     while target_label[i] == target[i]:
+        #         target_label[i] = random.randint(0, 9)
         
-        # normalizing adversarial perturbation
-        adv_max = torch.max(torch.max(adv_pertur, dim=-1)[0], dim=-1)[0]
-        adv_min = torch.min(torch.min(adv_pertur, dim=-1)[0], dim=-1)[0]
-        adv_mid = (adv_max + adv_min) / 2
-        adv_zero = (adv_max - adv_min) / 2
-        adv_norm = torch.ones_like(adv)
-        for i in range(len(adv_pertur)):
-            for j in range(len(adv_pertur[0])):
-                adv_norm[i][j] = (adv_pertur[i][j] - adv_mid[i][j].item()) / adv_zero[i][j].item()
-        adv_norm = epsilon * adv_norm
-        # min = torch.min(adv_pertur)
-        # max = torch.max(adv_pertur)
-        # mid = (max + min) / 2
-        # zero_mean = (max - min) / 2
-        # adv_pertur_norm = epsilon * (adv_pertur - mid) / zero_mean
-        adv_data = data.clone().detach() + adv_norm
-
-        guide_data, _ = guide_sample(guide_sets, target_label)
-        guide_data = guide_data.to(device)
-
-        # adversarial example output
-        adv_output = model(attack.generate(model, data, target))
 
         train_loss = F.nll_loss(output, target)
-        guided_loss = F.mse_loss(adv_data, guide_data)
-        adv_loss = attack.lf(adv_output, target)
         loss = (1-beta-gamma)*train_loss + beta*guided_loss + gamma*adv_loss
         loss.backward()
         optimizer.step()

example.py
@@ -550,68 +550,68 @@ def main():
 
     
     
-    print("\nAdversarial training (PGD):")
-    if args.load_model:
-        model.load_state_dict(torch.load("mnist_cnn_pgd.pt"))
-    else:
-        model.load_state_dict(start_point)
-        optimizer = optim.SGD(model.parameters(), lr=args.lr)
-        scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
-        pgd = ProjectedGradientDescent(lf=F.nll_loss, eps=args.eps, alpha=args.alpha, iter_max=args.iter_max)
-        adv_method = AdversarialTrain(model, device, train_loader, optimizer, attack=pgd)
-        model_training(args, model, adv_method, device, test_loader, scheduler)
-        if args.save_model:
-            torch.save(model.state_dict(), "mnist_cnn_{}.pt".format(pgd.name))
-    evaluation(args, model, device, test_loader)
-
-
-    # print("\nAdversarial guided training:")
+    # print("\nAdversarial training (PGD):")
     # if args.load_model:
-    #     model.load_state_dict(torch.load("mnist_cnn_adv_guided.pt"))
+    #     model.load_state_dict(torch.load("mnist_cnn_pgd.pt"))
     # else:
     #     model.load_state_dict(start_point)
-    #     optimizer = optim.SGD(model.parameters(), lr=100*args.lr)
+    #     optimizer = optim.SGD(model.parameters(), lr=args.lr)
     #     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
-    #     guide_sets = make_guide_set(train_set, size=1000)
-    #     adv_guided_method = AdversarialGuidedTrain(model, 
-    #                             device, 
-    #                             train_loader, 
-    #                             optimizer, 
-    #                             guide_sets=guide_sets, 
-    #                             epsilon=args.eps, 
-    #                             beta=args.beta, 
-    #                             weight_decay=args.weight_decay, 
-    #                             gradient_decay=args.gradient_decay)
-    #     model_training(args, model, adv_guided_method, device, test_loader, scheduler)
+    #     pgd = ProjectedGradientDescent(lf=F.nll_loss, eps=args.eps, alpha=args.alpha, iter_max=args.iter_max)
+    #     adv_method = AdversarialTrain(model, device, train_loader, optimizer, attack=pgd)
+    #     model_training(args, model, adv_method, device, test_loader, scheduler)
     #     if args.save_model:
-    #         torch.save(model.state_dict(), "mnist_cnn_adv_guided.pt")
+    #         torch.save(model.state_dict(), "mnist_cnn_{}.pt".format(pgd.name))
     # evaluation(args, model, device, test_loader)
-    
-    
-    print("\nAdversarial guided training + Adversarial training (PGD)")
+
+
+    print("\nAdversarial guided training:")
     if args.load_model:
-        model.load_state_dict(torch.load("mnist_cnn_adv_guided_pgd.pt"))
+        model.load_state_dict(torch.load("mnist_cnn_adv_guided.pt"))
     else:
         model.load_state_dict(start_point)
         optimizer = optim.SGD(model.parameters(), lr=args.lr)
         scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
         guide_sets = make_guide_set(train_set, size=1000)
-        pgd = ProjectedGradientDescent(lf=F.nll_loss, eps=args.eps, alpha=args.alpha, iter_max=args.iter_max)
-        adv_guided_pgd_method = AdversarialGuidedPGDTrain(model, 
+        adv_guided_method = AdversarialGuidedTrain(model, 
                                 device, 
                                 train_loader, 
                                 optimizer, 
-                                attack=pgd, 
                                 guide_sets=guide_sets, 
                                 epsilon=args.eps, 
                                 beta=args.beta, 
-                                gamma=args.ygamma, 
                                 weight_decay=args.weight_decay, 
                                 gradient_decay=args.gradient_decay)
-        model_training(args, model, adv_guided_pgd_method, device, test_loader, scheduler)
+        model_training(args, model, adv_guided_method, device, test_loader, scheduler)
         if args.save_model:
-            torch.save(model.state_dict(), "mnist_cnn_adv_guided_pgd.pt")
+            torch.save(model.state_dict(), "mnist_cnn_adv_guided.pt")
     evaluation(args, model, device, test_loader)
+    
+    
+    # print("\nAdversarial guided training + Adversarial training (PGD)")
+    # if args.load_model:
+    #     model.load_state_dict(torch.load("mnist_cnn_adv_guided_pgd.pt"))
+    # else:
+    #     model.load_state_dict(start_point)
+    #     optimizer = optim.SGD(model.parameters(), lr=args.lr)
+    #     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+    #     guide_sets = make_guide_set(train_set, size=1000)
+    #     pgd = ProjectedGradientDescent(lf=F.nll_loss, eps=args.eps, alpha=args.alpha, iter_max=args.iter_max)
+    #     adv_guided_pgd_method = AdversarialGuidedPGDTrain(model, 
+    #                             device, 
+    #                             train_loader, 
+    #                             optimizer, 
+    #                             attack=pgd, 
+    #                             guide_sets=guide_sets, 
+    #                             epsilon=args.eps, 
+    #                             beta=args.beta, 
+    #                             gamma=args.ygamma, 
+    #                             weight_decay=args.weight_decay, 
+    #                             gradient_decay=args.gradient_decay)
+    #     model_training(args, model, adv_guided_pgd_method, device, test_loader, scheduler)
+    #     if args.save_model:
+    #         torch.save(model.state_dict(), "mnist_cnn_adv_guided_pgd.pt")
+    # evaluation(args, model, device, test_loader)
 
 
 def make_guide_set(dataset, size=1):
@@ -620,7 +620,7 @@ def make_guide_set(dataset, size=1):
     for i in range(10):
         subset_index = []
         count = 0
-        while count < 1000:
+        while count < size:
             rand_index = random.randint(0, len(dataset) - 1)
             if dataset[rand_index][1] == i:
                 subset_index.append(rand_index)